a14dfd0914c25f771c759c2e7999348327f36d5b
Liquidburn2 <70215127+Liquidburn1@users.noreply.github.com>
2024-02-09 17:51:00 -0600

    env file

---
+++ src/pages/auth pages/my_routines.tsx
@@ -0,0 +1,16 @@
+function my_routine() {
+  return (
+    <div
+      style={{
+        backgroundSize: "cover",
+        backgroundPosition: "left top",
+        height: "100%",
+        overflow: "scroll",
+        // backgroundColor: "#2d3238",
+        backgroundColor: "white",
+      }}
+    ></div>
+  );
+}
+
+export default my_routine;
